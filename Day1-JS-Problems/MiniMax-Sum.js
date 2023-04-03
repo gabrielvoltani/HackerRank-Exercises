4186aa7d39f449cc7bf09865a01a11ecb52e3cd4
gabrielvoltani <gabriel.voltani@gmail.com>
2023-04-03 19:42:40 -0300

    Feat: MiniMax-Sum problem

---
+++ Day1-JS-Problems/MiniMax-Sum.js
@@ -0,0 +1,29 @@
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+// Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+function miniMaxSum(arr) {
+
+  const highArray = [...arr];
+  const lowArray = [...arr];
+  
+  const highestNum = Math.max(...arr);
+  const lowestNum = Math.min(...arr);
+  
+  const indexOfHigh = arr.indexOf(highestNum)
+  const indexOfLow = arr.indexOf(lowestNum)
+  
+  lowArray.splice(indexOfHigh, 1);
+  highArray.splice(indexOfLow, 1);
+  
+  
+  const lowSum = lowArray.reduce((acc, curr) => acc + curr, 0);
+  const highSum = highArray.reduce((acc, curr) => acc + curr, 0);
+  
+  
+  console.log(lowSum, highSum);
+  
+  }
+
+const numbers = [1, 7, 10, 13, 8]
+
+miniMaxSum(numbers);
