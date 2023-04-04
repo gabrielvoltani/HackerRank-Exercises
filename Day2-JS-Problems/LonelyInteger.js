4a418c89209d6e9d7c94ad81f1cb1a1d590c6192
gabrielvoltani <gabriel.voltani@gmail.com>
2023-04-04 18:43:14 -0300

    Feat: LonelyInteger problem

--- Day2-JS-Problems/LonelyInteger.js
+++ Day2-JS-Problems/LonelyInteger.js
@@ -1,1 +1,16 @@
 // Given an array of integers, where all elements but one occur twice, find the unique element.
+
+function lonelyinteger(a) {
+  for (let i = 0; i < a.length; i++) {
+      if (a.indexOf(a[i]) === a.lastIndexOf(a[i])) {
+          return a[i];
+      }
+  };
+}
+
+const array = [1, 1, 6, 6, 8, 8, 18];
+
+console.log(lonelyinteger(array));
+
+// My notes:
+// With this problem, i was able to remember the usage of "lastIndexOf" as a way to compare the number in the array
