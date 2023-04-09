c04a059abaee288e0323b885c7b356f87f3e86c4
gabrielvoltani <gabriel.voltani@gmail.com>
2023-04-09 15:42:54 -0300

    Feat: ZigZag problem

---
+++ Day3-JS-Problems/ZigZag.js
@@ -0,0 +1,5 @@
+// Given an array of N distinct integers, transform the array into a zig zag sequence by permuting the array elements.
+// A sequence will be called a zig zag sequence if the first K elements in the sequence are in increasing order and the last K elements are in decreasing order, where K = (N+1)/2.
+// You need to find the lexicographically smallest zig zag sequence of the given array.
+
+const a = 'b';
